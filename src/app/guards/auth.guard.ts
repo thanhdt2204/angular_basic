9b6c71974f8832c14620a0cc51d932ac690ce350
ThanhDoan <thanhdt2204@gmail.com>
2022-08-27 21:23:48 +0700

    Complete auth guard for routing

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Constants } from '../utils/constant';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { take, lastValueFrom } from 'rxjs';
+import { selectStore } from '../store/selectors/store.selector';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, private store: Store) { }
 
     async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        // const store = await this.storeService.currentStore.toPromise().then(store => { return store });
-        // if (store?.isLoggedIn) {
-        //     return true;
-        // }
+        const store = await lastValueFrom(this.store.pipe(select(selectStore), take(1)));
+        if (store?.isLoggedIn) {
+            return true;
+        }
 
-        // this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-        return true;
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        return false;
     }
 }
